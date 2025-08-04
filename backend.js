3a2871b79d1b72c534bc52b1b06782f07b9b8f78
TechLeon001 <leonardnaibei92@gmail.com>
2025-08-04 23:46:48 +0300

    linkung back and frintend

---
+++ backend.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const app = express();
+
+app.post("/api/users",async(req,res)=>{
+    const user = await User.create(req.body);
+    res.json(user);
+})
---
+++ frontend.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+const API = "http://localhost:5000/api/users";
+
+
+const createUser = async()=>{
+    const res = await axios.post(API,{
+        name : "leonard"
+    });
+    console.log(res.data) // show created user
+};
